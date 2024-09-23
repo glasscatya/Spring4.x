7139646380e59a1576cc6915e599b36dd9bf3689
yufang <2721381743@qq.com>
2024-09-23 20:57:37 +0800

    complete TestNG

---
+++ src/test/java/com/smart/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package com.smart.service;
+
+import com.smart.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+
+import static org.testng.Assert.*;
+
+@TransactionConfiguration(defaultRollback = false)
+@ContextConfiguration("classpath*:/smart-context.xml")
+public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testHasMatchUser() {
+        boolean b1 = userService.hasMatchUser("admin", "123456");
+        boolean b2 = userService.hasMatchUser("admin", "1111");
+        assertTrue(b1);
+        assertTrue(!b2);
+    }
+
+    @Test
+    public void testFindUserByUserName()throws Exception{
+        for(int i =0; i< 100;i++) {
+            User user = userService.findUserByUserName("admin");
+            assertEquals(user.getUserName(), "admin");
+        }
+    }
+
+    @Test
+    public void testAddLoginLog() {
+        User user = userService.findUserByUserName("admin");
+        System.out.println(user);
+        user.setUserId(1);
+        user.setUserName("admin");
+        user.setLastIp("192.168.12.7");
+        user.setLastVisit(new Date());
+        userService.loginSuccess(user);
+    }
+
+
+
+}
